5b324281e563ff78a8fdd5882ea735b1a7f16463
Bryan Burnside <bpburnside@gmail.com>
2024-12-10 14:40:24 -0600

    Complete pt 1 and create absolute value utility function

--- day01/main.go
+++ day01/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/bryanpburnside/advent_of_code_2024/utils"
 )
@@ -12,6 +15,36 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	output := getDifference(input)
+	fmt.Println(output)
+}
 
-	fmt.Println("Puzzle input:", input)
+func getDifference(input []string) int {
+	difference := int(0)
+	leftList := []int{}
+	rightList := []int{}
+
+	for _, line := range input {
+		split := strings.Split(line, "   ")
+		numberLeft, err := strconv.Atoi(split[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		numberRight, err := strconv.Atoi(split[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		leftList = append(leftList, numberLeft)
+		rightList = append(rightList, numberRight)
+	}
+
+	sort.Ints(leftList)
+	sort.Ints(rightList)
+
+	for i, leftNumber := range leftList {
+		rightNumber := rightList[i]
+		difference += utils.Abs(leftNumber - rightNumber)
+	}
+
+	return difference
 }
--- utils/utils.go
+++ utils/utils.go
@@ -20,3 +20,11 @@ func ReadFile(filename string) ([]string, error) {
 	}
 	return lines, scanner.Err()
 }
+
+// Abs returns the absolute value of an integer.
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
